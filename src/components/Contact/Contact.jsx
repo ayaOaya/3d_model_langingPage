90620d606edeed209c87c21a703a8f2c185786b2
ayaOaya <alyaauaacn@gmail.com>
2023-03-08 21:30:20 +0100

    fith commit

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,6 +2,20 @@ import React from 'react'
 
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <div>
+      <h1>Fell free to reach out </h1>
+
+
+      <section>
+        <h5>YOUR NAME</h5>
+        <input type="text"  required/>
+        <h5>YOUR LAST NAME</h5>
+        <input type="text"  required/>
+        <h5>YOUR EMAIL</h5>
+        <input type="text"  required/>
+        <h5>YOUR MESSAGE</h5>
+        <input type="text" />
+      </section>
+    </div>
   )
 }
